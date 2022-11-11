e32569a0d47ae8f3d35f8a471fefd78acbaf1eba
basm1610 <86631963+basm1610@users.noreply.github.com>
2022-11-11 15:36:59 +0200

    new image

--- test.java
+++ test.java
@@ -1,10 +1,6 @@
-public class test{
-
-public static void main(main[] args){
-
-System.out.println("Basm Mohamed Abdel Fatteh");
-
-}
-
-
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Basm Mohamed Abdel Fatteh");
+    }
+    
 }
